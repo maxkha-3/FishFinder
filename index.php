8782b4ebb918aaa04004fc6c94df09eb0674e1c8
maxkha-3 <maxkha-3@student.ltu.se>
2016-11-07 15:56:52 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+	error_reporting(E_ALL);	
+	
+	session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+      #map {
+        height: 400px;
+        width: 100%;
+       }
+    </style>
+  </head>
+  <body>
+    <h3>My Google Maps Demo</h3>
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+		ga('create', 'UA-86978734-1', 'auto');
+		ga('send', 'pageview');
+
+	</script>
+    <div id="map"></div>
+	
+    <script>
+      function initMap() {
+        var uluru = {lat: -25.363, lng: 131.044};
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 4,
+          center: uluru
+        });
+        var marker = new google.maps.Marker({
+          position: uluru,
+          map: map
+        });
+      }
+    </script>
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAquCRzK53TvC2xsUdMM41A12eWiLfLS4k&callback=initMap">
+    </script>
+  </body>
+</html>
